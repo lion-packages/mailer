db0f33b6296c3e1c0723a8fafc96603b1a9337e4
Sergio León <56183278+Sleon4@users.noreply.github.com>
2022-03-07 16:14:23 -0500

    first commit.

---
+++ src/LionMailer/Attach.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace LionMailer;
+
+class Attach implements \JsonSerializable {
+	
+	
+	
+}
---
+++ src/LionMailer/Mailer.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace LionMailer;
+
+class Mailer {
+	
+	public function __construct() {
+		
+	}
+	
+}
